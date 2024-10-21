5e22cb00ebd2b2204e8c80884c686936cd78f397
caremackin <cmackin712@gmail.com>
2024-10-21 16:31:05 -0400

    Initial commit

---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+import { fakeFetch } from "src/utils/fetch"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const [approved, setApproved] = useState(transaction.approved)
+
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+          try {
+            await consumerSetTransactionApproval({
+              transactionId: transaction.id,
+              newValue,
+            })
+            setApproved(newValue)
+          } catch (error) {
+            console.error("Failed to update approval status:", error)
+          }
+        }}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useState } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions }) => {
+  const { fetchWithoutCache, loading } = useCustomFetch()
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      try {
+        await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+          transactionId,
+          value: newValue,
+        })
+      } catch (error) {
+        console.error("Failed to set transaction approval", error)
+      }
+    },
+    [fetchWithoutCache]
+  )
+
+  if (transactions === null) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+        />
+      ))}
+    </div>
+  )
+}
